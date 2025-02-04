911253e475d1c1476ac1df468dbf87731471f20b
Ananthakrishnan <71790345+Ananthu26@users.noreply.github.com>
2025-02-04 11:24:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
         const getWeather = async (city) => {
             const apiKey = '3e9b6a717cf9d33ac0b1e8be45b01133';
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+            const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
             const data = await response.json();
             console.log(data);
 
-            document.getElementById('weather-icon').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+            document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
             document.getElementById('weather-city').innerText = data.name;
             document.getElementById('weather-description').innerText = data.weather[0].description;
             document.getElementById('weather-temp').innerText = `Temperature: ${data.main.temp} °C`;
@@ -17,22 +17,22 @@
 
             switch (weatherCondition) {
                 case 'clear':
-                    backgroundImage = 'url(https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg)';
+                    backgroundImage = 'url(http://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg)';
                     break;
                 case 'clouds':
-                    backgroundImage = 'url(https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg)';
+                    backgroundImage = 'url(http://images.pexels.com/photos/414171/pexels-photo-414171.jpeg)';
                     break;
                 case 'rain':
-                    backgroundImage = 'url(https://images.pexels.com/photos/110874/pexels-photo-110874.jpeg)';
+                    backgroundImage = 'url(http://images.pexels.com/photos/110874/pexels-photo-110874.jpeg)';
                     break;
                 case 'snow':
-                    backgroundImage = 'url(https://images.pexels.com/photos/60561/winter-snow-nature-60561.jpeg)';
+                    backgroundImage = 'url(http://images.pexels.com/photos/60561/winter-snow-nature-60561.jpeg)';
                     break;
                 case 'thunderstorm':
-                    backgroundImage = 'url(https://images.pexels.com/photos/1118869/pexels-photo-1118869.jpeg)';
+                    backgroundImage = 'url(http://images.pexels.com/photos/1118869/pexels-photo-1118869.jpeg)';
                     break;
                 default:
-                    backgroundImage = 'url(https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg)';
+                    backgroundImage = 'url(http://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg)';
                     break;
             }
 
